25207ee7f819097561aea9ad81a4008960d5479b
tej <tejakiran.yedlapally@.com>
2023-11-21 23:19:05 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Replace 'your-app-filename.apk' with the actual filename of your APK.
+    var apkFilename = 'dog.apk';
+    
+    // Replace 'path/to/your/apk/' with the actual path to your APK file.
+    var apkPath = 'app/' + apkFilename;
+
+    // Combine the path and filename to create the full download link.
+    var downloadLink = apkPath;
+
+    // Redirect the user to the download link.
+    window.location.href = downloadLink;
+});
